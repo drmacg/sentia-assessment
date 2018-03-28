71a0fafff4c34c1f8d4509f357fd23fa298f248d
gbosnjak <bosnjak.gabrijela@gmail.com>
2018-03-28 19:55:15 +0200

    Improve code with RuboCop

--- assignment.rb
+++ assignment.rb
@@ -1,35 +1,35 @@
 require 'json'
 
 template = {
-  :AWSTemplateFormatVersion => "2010-09-09",
-  :Outputs => {
-    :PublicIP => {
-      :Description => "Public IP address of the newly created EC2 instance",
-      :Value => {
-        :"Fn::GetAtt" => ["EC2Instance", "PublicIp"]
+  AWSTemplateFormatVersion: '2010-09-09',
+  Outputs: {
+    PublicIP: {
+      Description: 'Public IP address of the newly created EC2 instance',
+      Value: {
+        :"Fn::GetAtt" => %w[EC2Instance PublicIp]
       }
     }
   },
-  :Resources => {
-    :EC2Instance => {
-      :Properties => {
-        :ImageId => "ami-b97a12ce",
-        :InstanceType => "t2.micro",
-        :SecurityGroups => [:Ref => "InstanceSecurityGroup"]
+  Resources: {
+    EC2Instance: {
+      Properties: {
+        ImageId: 'ami-b97a12ce',
+        InstanceType: 't2.micro',
+        SecurityGroups: [Ref: 'InstanceSecurityGroup']
       },
-      :Type => "AWS::EC2::Instance"
+      Type: 'AWS::EC2::Instance'
     },
-    :InstanceSecurityGroup => {
-      :Properties => {
-        :GroupDescription => "Enable SSH access via port 22",
-        :SecurityGroupIngress => [{
-          :CidrIp => "0.0.0.0/0",
-          :FromPort => "22",
-          :IpProtocol => "tcp",
-          :ToPort => "22"
+    InstanceSecurityGroup: {
+      Properties: {
+        GroupDescription: 'Enable SSH access via port 22',
+        SecurityGroupIngress: [{
+          CidrIp: '0.0.0.0/0',
+          FromPort: '22',
+          IpProtocol: 'tcp',
+          ToPort: '22'
         }]
       },
-      :Type => "AWS::EC2::SecurityGroup"
+      Type: 'AWS::EC2::SecurityGroup'
     }
   }
 }
