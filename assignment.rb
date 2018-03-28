ec79eeb62ceace1aef597d08128f99751cab0d32
gbosnjak <bosnjak.gabrijela@gmail.com>
2018-03-28 19:44:53 +0200

    Refactoring template

--- assignment.rb
+++ assignment.rb
@@ -1,50 +1,37 @@
 require 'json'
 
-
-properties = {
-  :ImageId => "ami-b97a12ce",
-  :InstanceType => "t2.micro",
-  :SecurityGroups => [:Ref => "InstanceSecurityGroup"]
-}
-ec2instance = {
-  :Properties => properties,
-  :Type => "AWS::EC2::Instance"
-}
-securityGroupIngress = {
-  :CidrIp => "0.0.0.0/0",
-  :FromPort => "22",
-  :IpProtocol => "tcp",
-  :ToPort => "22"
-}
-props = {
-  :GroupDescription => "Enable SSH access via port 22",
-  :SecurityGroupIngress => [securityGroupIngress]
-}
-instancesecuritygroup = {
-  :Properties => props,
-  :Type => "AWS::EC2::SecurityGroup"
-}
-resources = {
-  :EC2Instance => ec2instance,
-  :InstanceSecurityGroup => instancesecuritygroup
-}
-
-value = {
-  :"Fn::GetAtt" => ["EC2Instance", "PublicIp"]
-}
-publicIP = {
-  :Description => "Public IP address of the newly created EC2 instance",
-  :Value => value
-}
-output = {
-  :PublicIP => publicIP
-}
 template = {
   :AWSTemplateFormatVersion => "2010-09-09",
-  :Outputs => output,
-  :Resources => resources
-
+  :Outputs => {
+    :PublicIP => {
+      :Description => "Public IP address of the newly created EC2 instance",
+      :Value => {
+        :"Fn::GetAtt" => ["EC2Instance", "PublicIp"]
+      }
+    }
+  },
+  :Resources => {
+    :EC2Instance => {
+      :Properties => {
+        :ImageId => "ami-b97a12ce",
+        :InstanceType => "t2.micro",
+        :SecurityGroups => [:Ref => "InstanceSecurityGroup"]
+      },
+      :Type => "AWS::EC2::Instance"
+    },
+    :InstanceSecurityGroup => {
+      :Properties => {
+        :GroupDescription => "Enable SSH access via port 22",
+        :SecurityGroupIngress => [{
+          :CidrIp => "0.0.0.0/0",
+          :FromPort => "22",
+          :IpProtocol => "tcp",
+          :ToPort => "22"
+        }]
+      },
+      :Type => "AWS::EC2::SecurityGroup"
+    }
+  }
 }
-
 
 puts JSON.pretty_generate(template)
