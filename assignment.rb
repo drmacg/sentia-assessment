035a8f9c2e8efdcc0bdec39261e1cc7476b72126
gbosnjak <bosnjak.gabrijela@gmail.com>
2018-04-02 09:24:12 +0200

    Extracting parseArguments method

--- assignment.rb
+++ assignment.rb
@@ -2,19 +2,7 @@ require 'json'
 require 'argv'
 require_relative 'functions'
 
-arguments = ARGV.to_hash
-
-instances = arguments['instances']
-instanceType = arguments['instance-type']
-ipAddress = arguments['allow-ssh-from']
-
-instanceType = 't2.micro' if instanceType.nil?
-
-ipAddress = if ipAddress.nil?
-              '0.0.0.0/0'
-            else
-              ipAddress + '/32'
-            end
+instances, instanceType, ipAddress = parseArguments()
 
 template = createTemplate(instanceType, ipAddress, instances)
 
--- functions.rb
+++ functions.rb
@@ -1,3 +1,20 @@
+def parseArguments()
+  arguments = ARGV.to_hash
+
+  instances = arguments['instances']
+  instanceType = arguments['instance-type']
+  ipAddress = arguments['allow-ssh-from']
+
+  instanceType = 't2.micro' if instanceType.nil?
+
+  ipAddress = if ipAddress.nil?
+                '0.0.0.0/0'
+              else
+                ipAddress + '/32'
+              end
+  return instances, instanceType, ipAddress
+end
+
 def createTemplate(instanceType, ipAddress, instances)
   ec2InstanceTemplate = {
     Properties: {
